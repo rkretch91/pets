52b7e0c5a596a4e7c044358e1e9eed42b2bbc625
Ryan Kretch <ryan.kretch@door2door.io>
2022-01-15 09:13:18 -0800

    finish rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  # READ(ALL)
+  get 'pets', to: 'pets#index'
+
+  # CREATE
+  get 'pets/new', to: 'pets#new'
+  post 'pets', to: 'pets#create'
+
+  # UPDATE
+  get 'pets/:id/edit', to: 'pets#edit', as: :pets_edit
+  patch 'pets/:id', to: 'pets#update'
+
+  # DESTROY
+  delete 'pets/:id', to: 'pets#destroy'
+
+  # READ(ONE)
+  get 'pets/:id', to: 'pets#show', as: :pet
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
